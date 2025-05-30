c748dc22677e164460510e23259ab8bdd86bbc4d
VladimirKBG <owl2666@mail.ru>
2025-05-31 00:50:29 +0500

    Create FirstTests.java

---
+++ src/test/java/test/FirstTests.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author user
+ */
+public class FirstTests {
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
